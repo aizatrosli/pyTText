8777d8dd7e726989eca466a21c93d48105ff2606
Ahmad Aizat <ahmadaizatr@gmail.com>
2020-07-16 13:01:24 +0800

    using tabulate to print

--- pyttext.py
+++ pyttext.py
@@ -2,8 +2,11 @@ import argparse,os,sys
 from pyTText.core import *
 from pyTText.model import *
 from pyTText.utils import *
+from tabulate import tabulate
+import random
 import getpass
 
+random.seed(100)
 parser = argparse.ArgumentParser(description="pyTText: twitter sentiment analysis!")
 
 
@@ -11,16 +14,16 @@ def demo():
     print('{0}{1}{0}'.format('#'*20,'Load demo tweet dataset'))
     df = pd.read_csv('./data/tweets.csv')
     dft = df[['handle', 'text', 'is_retweet', 'original_author', 'time', 'lang', 'retweet_count', 'favorite_count']]
-    print(dft.head())
+    print(tabulate(dft, headers='keys', tablefmt='psql'))
     print('{0}{1}{0}'.format('#' * 20, 'Preprocessing dataset'))
     tlib = TextLibrary()
     pattern = tlib.patternlist()
     ttransform = TextTransform(pattern, backup='oritext')
     tweetdf = ttransform.preprocess(dft)
-    print(tweetdf.head().to_markdown())
+    print(tabulate(tweetdf, headers='keys', tablefmt='psql'))
     print('{0}{1}{0}'.format('#' * 20, 'Scoring tweet using lexicon'))
     tweetscoredf = ttransform.opinionscore(tweetdf, remove='trump')
-    print(tweetscoredf.head().to_markdown())
+    print(tabulate(tweetscoredf, headers='keys', tablefmt='psql'))
     print('{0}{1}{0}'.format('#' * 20, 'Creating DTM'))
     tokencol = ttransform.splituser(tweetscoredf)
     dtm = ttransform.dtm
@@ -29,11 +32,12 @@ def demo():
     tr.summarysentiment()
     print('{0}{1}{0}'.format('#' * 20, 'Training Model'))
     tr.train()
-    print(pd.DataFrame(tr.model).to_markdown())
+
+    print(tr.summarymetric().to_markdown())
     print('{0}{1}{0}'.format('#' * 20, 'Inferencing Model'))
     ti = TInfer(tr.sessions)
     text = input("Enter any text/sentence: ")
-    bestmodel = ti.sessions['realDonaldTrump'].bestmodel
+    bestmodel = ti.sessions[random.choice(list(tr.sessions.keys()))].bestmodel
     print(ti.predict(text, bestmodel))
 
 
@@ -54,7 +58,7 @@ def tweetdemo():
 
 group1 = parser.add_mutually_exclusive_group(required=True)
 group1.add_argument('--twitterdemo',action="store_true", help="Twitter live demo scraping.")
-group1.add_argument('--demo',action="store_false", help="Simple demo training, testing and inferencing.")
+group1.add_argument('--demo', action="store_false", help="Simple demo training, testing and inferencing.")
 args = parser.parse_args()
 
 if args.twitterdemo:
